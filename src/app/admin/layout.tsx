2a31268b7d0a6d4415a45f6b85eef90f29b53801
reynaldo-dev11 <withcenter.dev11@gmail.com>
2025-06-12 17:37:14 +0800

    added custom metdata on admin page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,25 @@
 // src/app/admin/layout.tsx
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "./sidebar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin Page",
+  description: "Learn more about our admin platform.",
+  openGraph: {
+    title: "About Us",
+    description: "Learn more about our admin platform.",
+    images: [
+      {
+        url: "https://chia-azure.vercel.app/images/fallback-image.png",
+        width: 800,
+        height: 600,
+        alt: "About OG Image",
+      },
+    ],
+    type: "website",
+  },
+};
 
 export default function AdminLayout({
   children,
