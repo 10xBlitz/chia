e6551890d5bb8c47c03086d5968d0a9fa11181b8
reynaldo-dev11 <withcenter.dev11@gmail.com>
2025-06-12 17:12:55 +0800

    moved root page file into root directory

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use server";
+
+import MainPage from "./(main)/main-content";
+
+async function Page() {
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate delay
+  return <MainPage />;
+}
+
+export default Page;
