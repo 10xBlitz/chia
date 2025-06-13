75e5eceaae182aebd1049fe9eff41f9df7be698f
reynaldo-dev11 <withcenter.dev11@gmail.com>
2025-06-13 10:39:02 +0800

    changed chia logo into png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,10 @@ export default function RootLayout({
           content="치과 시술 견적 및 예약 플랫폼"
         />
         <meta property="og:siteName" content="치과 시술 플랫폼" />
-        <meta property="og:image" content="https://chia-azure.vercel.app/images/chia-logo.svg" />
+        <meta
+          property="og:image"
+          content="https://chia-azure.vercel.app/images/chia-logo.png"
+        />
         <meta property="og:image:width" content="54" />
         <meta property="og:image:height" content="24" />
       </head>
