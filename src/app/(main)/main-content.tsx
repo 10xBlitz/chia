a72ef7ca0b1e72014dc4ffb0655e2fed5b50469e
reynaldo-dev11 <withcenter.dev11@gmail.com>
2025-06-13 09:01:44 +0800

    added og tags in header

--- src/app/(main)/main-content.tsx
+++ src/app/(main)/main-content.tsx
@@ -26,30 +26,6 @@ import { Button } from "@/components/ui/button";
 import ClinicCardSkeleton from "@/components/loading-skeletons/clinic-card-skeleton";
 import { getPaginatedClinicsWthReviews } from "@/lib/supabase/services/clinics.services";
 import Footer from "@/components/footer";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "치과 시술 플랫폼", // "Dental Procedure Platform"
-  description: "치과 시술 견적 및 예약 플랫폼", // "Dental Procedure Quotation and Reservation Platform"
-  icons: {
-    icon: "/images/chia-logo.svg",
-  },
-  openGraph: {
-    title: "치과 시술 플랫폼", // "Dental Procedure Platform"
-    description: "치과 시술 견적 및 예약 플랫폼", // "Dental Procedure Quotation and Reservation Platform"
-    siteName: "치과 시술 플랫폼",
-    images: [
-      {
-        url: "https://hmhtqgzcqoxssuhtmscp.supabase.co/storage/v1/object/public/clinic-images//2471d7e1-f871-4bdd-ba84-9755cf7f38f3.png", // Absolute URL for OG image
-        width: 800,
-        height: 600,
-        alt: "My custom alt",
-      },
-    ],
-    locale: "ko_KR",
-    type: "website",
-  },
-};
 
 export default function MainPage() {
   const searchParams = useSearchParams();
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,7 @@ import "./fonts.css";
 import { UserStoreProvider } from "@/providers/user-store-provider";
 import CustomQueryClientProvider from "@/providers/query-client-provider";
 import { ToasterProvider } from "@/providers/toast-provider";
-// type Props = {
-//   params: Promise<{ id: string }>;
-//   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-// };
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,29 +15,28 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-//dynamic (SSR)
-
-//static metadata for the root layout
-// export async function generateMetadata(
-//   { params }: Props,
-//   parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//   // read route params
-//   const { id } = await params;
-
-//   // fetch data
-//   const product = await fetch(`https://.../${id}`).then((res) => res.json());
-
-//   // optionally access and extend (rather than replace) parent metadata
-//   const previousImages = (await parent).openGraph?.images || [];
-
-//   return {
-//     title: product.title,
-//     openGraph: {
-//       images: ["/some-specific-page-image.jpg", ...previousImages],
-//     },
-//   };
-// }
+// export const metadata: Metadata = {
+//   title: "치과 시술 플랫폼", // "Dental Procedure Platform"
+//   description: "치과 시술 견적 및 예약 플랫폼", // "Dental Procedure Quotation and Reservation Platform"
+//   icons: {
+//     icon: "/images/chia-logo.svg",
+//   },
+//   openGraph: {
+//     title: "치과 시술 플랫폼", // "Dental Procedure Platform"
+//     description: "치과 시술 견적 및 예약 플랫폼", // "Dental Procedure Quotation and Reservation Platform"
+//     siteName: "치과 시술 플랫폼",
+//     images: [
+//       {
+//         url: "https://hmhtqgzcqoxssuhtmscp.supabase.co/storage/v1/object/public/clinic-images//2471d7e1-f871-4bdd-ba84-9755cf7f38f3.png", // Absolute URL for OG image
+//         width: 800,
+//         height: 600,
+//         alt: "My custom alt",
+//       },
+//     ],
+//     locale: "ko_KR",
+//     type: "website",
+//   },
+// };
 
 export default function RootLayout({
   children,
@@ -49,6 +45,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          property="og:image"
+          content="https://hmhtqgzcqoxssuhtmscp.supabase.co/storage/v1/object/public/clinic-images//2471d7e1-f871-4bdd-ba84-9755cf7f38f3.png"
+        />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="600" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
