ef95233eefaf295d4738e81da2c8e4794b598f71
reynaldo-dev11 <withcenter.dev11@gmail.com>
2025-06-16 09:04:14 +0800

    added custom metadata in home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,25 @@
-"use server";
 import MobileLayout from "@/components/layout/mobile-layout";
 import MainPage from "./main-component";
+
+export const metadata = {
+  title: "치과 시술 플랫폼", // "Dental Procedure Platform"
+  description: "치과 시술 견적 및 예약 플랫폼", // "Dental Procedure Quotation and Reservation Platform"
+  icons: {
+    icon: "https://chia-azure.vercel.app/images/chia-logo.svg",
+  },
+  openGraph: {
+    title: "치과 시술 플랫폼",
+    description: "치과 시술 견적 및 예약 플랫폼",
+    url: "https://chia-azure.vercel.app",
+    siteName: "치과 시술 플랫폼",
+    images: [
+      {
+        url: "https://hmhtqgzcqoxssuhtmscp.supabase.co/storage/v1/object/public/banner-images//banner-1.png",
+        alt: "치과 시술 플랫폼 로고",
+      },
+    ],
+  },
+};
 
 export default async function Page() {
   return (
