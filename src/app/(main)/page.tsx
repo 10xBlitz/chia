5864b25b8b960291f9f2e451825940f2f2168094
reynaldo-dev11 <withcenter.dev11@gmail.com>
2025-06-16 09:38:48 +0800

    added metadatabase url

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,18 @@
 import MobileLayout from "@/components/layout/mobile-layout";
 import MainPage from "./main-component";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://chia-azure.vercel.app"),
+  openGraph: {
+    images: [
+      {
+        url: "/opengraph-image.png",
+        alt: "치과 시술 플랫폼 로고",
+      },
+    ],
+  },
+};
 
 export default async function Page() {
   return (
