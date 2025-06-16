2bbc473e5e0684382f3ce4e3bfc5a5e43bd803f1
reynaldo-dev11 <withcenter.dev11@gmail.com>
2025-06-16 09:13:51 +0800

    changed title and description on custom metadata on home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,16 +2,12 @@ import MobileLayout from "@/components/layout/mobile-layout";
 import MainPage from "./main-component";
 
 export const metadata = {
-  title: "치과 시술 플랫폼", // "Dental Procedure Platform"
-  description: "치과 시술 견적 및 예약 플랫폼", // "Dental Procedure Quotation and Reservation Platform"
+  title: "aaa", // "Dental Procedure Platform"
+  description: "bbb", // "Dental Procedure Quotation and Reservation Platform"
   icons: {
     icon: "https://chia-azure.vercel.app/images/chia-logo.svg",
   },
   openGraph: {
-    title: "aaa",
-    description: "bbb",
-    url: "https://chia-azure.vercel.app",
-    siteName: "치과 시술 플랫폼",
     images: [
       {
         url: "https://hmhtqgzcqoxssuhtmscp.supabase.co/storage/v1/object/public/banner-images/banner-1.png", // fixed double slash
